6ee716ab36cc8fcae563967a9615cfa08e802983
xmenxman <xmenxman@163.com>
2020-09-28 22:38:42 +0800

    增加miniweb框架

--- quarter.py
+++ quarter.py
@@ -1,1 +1,20 @@
 import jieba
+import xlrd
+
+file_name = "close_door.xls"
+
+def read_excel():
+    wb = xlrd.open_workbook(filename=file_name)
+    sheet1 = wb.sheet_by_name('查询结果')
+    cols = sheet1.col_values(9)
+    txt = ''
+    for row_txt in cols:
+        jlist = jieba.lcut(row_txt)
+        print(jlist)
+    #     txt += row_txt
+    # print(txt)
+    # jlist = jieba.lcut(txt)
+
+
+if __name__ == '__main__':
+    read_excel()
